542945c192c7f2418df04928b6bfa5696be771a9
Simon Toth <happy.cerberus@gmail.com>
2020-10-17 17:16:11 +0200

    Stream update.

--- src/main.ts
+++ src/main.ts
@@ -2,8 +2,10 @@ import * as backbone from "./backbone/backbone"
 import * as twitch_chat from "./platforms/twitch/chat"
 import * as greeter from "./demo/greeter"
 import * as twitch_stats from "./platforms/twitch/steady_state"
+import * as obs_export from "./obs/stream_stats"
 
 backbone.run();
-twitch_chat.run("twitch_chat", "tcp://127.0.0.1:6001");
-greeter.run("greeter", "tcp://127.0.0.1:6002");
-twitch_stats.run("twitch_stats", "tcp://127.0.0.1:6003");
+//twitch_chat.run("twitch_chat", "tcp://127.0.0.1:6001");
+//greeter.run("greeter", "tcp://127.0.0.1:6002");
+//twitch_stats.run("twitch_stats", "tcp://127.0.0.1:6003");
+obs_export.run("obs_export", "tcp://127.0.0.1:6004");
--- src/obs/stream_stats.ts
+++ src/obs/stream_stats.ts
@@ -1,8 +1,7 @@
 import { parse } from "path"
 import * as zmq from "zeromq"
 import * as m from "../lib/types"
-import * as socketio from "socket.io"
-import * as http from "http"
+import * as ws from "ws"
 
 class StreamStatsData {
     constructor(public twitch_viewers: number,
@@ -12,11 +11,10 @@ class StreamStatsData {
 
 let latest_stats = new StreamStatsData(0, 0, "");
 
-async function periodic_export() {
-    const socket = socketio(3000);
+async function periodic_export(socket: ws) {
     while (true) {
         // serialize current latest_stats and send on the socket
-        socket.emit("hello world!");
+        socket.send("hello world!");
         await new Promise(resolve => setTimeout(resolve, 500));
     }
 }
@@ -27,39 +25,54 @@ export async function run(name: string, hostport: string) {
     sub.connect("tcp://127.0.0.1:6667");
     sub.subscribe();
 
-    periodic_export();
+    const sock = new ws.Server({
+        port: 3000,
+    });
+    sock.on("error", (error: Error) => {
+        console.log(`Socket error ${error}`);
+    });
+    sock.on("connection", (socket: ws, request: ws.MessageEvent) => {
+        console.log(`Received connection on socket ${request}`);
+        periodic_export(socket);
+    });
+    sock.on("close", () => {
+        console.log(`Socket disconnected.`);
+    });
 
-    for await (const [topic, msg] of sub) {
-        try {
-            switch (topic.toString()) {
-                case m.MessageTypes[m.MessageTypes.StreamStatsFollowers]:
-                    let followers = JSON.parse(msg.toString()) as m.StatsFollowers;
-                    switch (followers.platform) {
-                        case m.Platform.Twitch:
-                            latest_stats.twitch_followers = followers.followers;
-                            break;
-                    }
-                    break;
-                case m.MessageTypes[m.MessageTypes.StreamStatsLatestFollower]:
-                    let latest_follower = JSON.parse(msg.toString()) as m.StatsLatestFollower;
-                    switch (latest_follower.platform) {
-                        case m.Platform.Twitch:
-                            latest_stats.twitch_latest_follower = latest_follower.latest_follower;
-                            break;
-                    }
-                    break;
-                case m.MessageTypes[m.MessageTypes.StreamStatsViewers]:
-                    let viewers = JSON.parse(msg.toString()) as m.StatsViewers;
-                    switch (viewers.platform) {
-                        case m.Platform.Twitch:
-                            latest_stats.twitch_viewers = viewers.viewers;
-                            break;
-                    }
-                    break;
 
+    sock.on('connection', periodic_export);
+    /*
+        for await (const [topic, msg] of sub) {
+            try {
+                switch (topic.toString()) {
+                    case m.MessageTypes[m.MessageTypes.StreamStatsFollowers]:
+                        let followers = JSON.parse(msg.toString()) as m.StatsFollowers;
+                        switch (followers.platform) {
+                            case m.Platform.Twitch:
+                                latest_stats.twitch_followers = followers.followers;
+                                break;
+                        }
+                        break;
+                    case m.MessageTypes[m.MessageTypes.StreamStatsLatestFollower]:
+                        let latest_follower = JSON.parse(msg.toString()) as m.StatsLatestFollower;
+                        switch (latest_follower.platform) {
+                            case m.Platform.Twitch:
+                                latest_stats.twitch_latest_follower = latest_follower.latest_follower;
+                                break;
+                        }
+                        break;
+                    case m.MessageTypes[m.MessageTypes.StreamStatsViewers]:
+                        let viewers = JSON.parse(msg.toString()) as m.StatsViewers;
+                        switch (viewers.platform) {
+                            case m.Platform.Twitch:
+                                latest_stats.twitch_viewers = viewers.viewers;
+                                break;
+                        }
+                        break;
+    
+                }
+            } catch (err) {
+                console.log(`Failed to parse message ${msg.toString()} : ${err}`);
             }
-        } catch (err) {
-            console.log(`Failed to parse message ${msg.toString()} : ${err}`);
-        }
-    }
+        }*/
 }
